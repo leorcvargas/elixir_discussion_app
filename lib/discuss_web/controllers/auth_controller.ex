7195518ca9da7eb7d1712304ed2692e638e31b82
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2021-01-10 16:59:28 -0300

    Add signout logic

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -13,6 +13,12 @@ defmodule DiscussWeb.AuthController do
     signin(conn, user_params)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
+
   defp signin(conn, user_params) do
     case Discuss.Users.sign_up(user_params) do
       {:ok, user} ->
@@ -20,6 +26,7 @@ defmodule DiscussWeb.AuthController do
         |> put_flash(:info, "Welcome!")
         |> put_session(:user_id, user.id)
         |> redirect(to: Routes.topic_path(conn, :index))
+
       {:error, _reason} ->
         conn
         |> put_flash(:error, "Error signing in")
